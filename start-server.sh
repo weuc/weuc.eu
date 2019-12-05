f2fe87e7159cc4e75d8d1486d9b4f7363cea2600
Thomas Lohner <tl@scale.sc>
2019-12-05 20:36:52 +0100

    add host to docker command

--- start-server.sh
+++ start-server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm --volume="$PWD:/srv/jekyll" --volume="$PWD/.gems:/usr/local/bundle" -it -p 4000:4000 jekyll/jekyll:4.0 jekyll serve
+docker run --rm --volume="$PWD:/srv/jekyll" --volume="$PWD/.gems:/usr/local/bundle" -it -p 4000:4000 jekyll/jekyll:4.0 jekyll serve --host 127.0.0.1
